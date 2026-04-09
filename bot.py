5d952d8022e97c06fc2c4803350f684656f51e85
BlaszaQ <baj960745@gmail.com>
2026-04-09 21:37:51 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("MTQ2NTc5MjQ0Njg1OTQ0NDI0NA.GkfR5i.hOqId2shMNfWaAbsmlXhanpOceltQY2JUMTP-I")
+GUILD_ID = int(os.getenv("1478753133365690608"))
